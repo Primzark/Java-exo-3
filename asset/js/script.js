113f60621feebb973d06b3f245151f418b43422f
Primzark <starlod7696@gmail.com>
2024-12-09 14:27:20 +0100

    U

---
+++ asset/js/script.js
@@ -0,0 +1,68 @@
+
+/*
+let mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+console.table(mois);
+
+
+
+let mois2 = new Array;
+mois2[0] = "Janvier";
+mois2[1] = "Février";
+mois2[2] = "Mars";
+mois2[3] = "Avril";
+mois2[4] = "Mai";
+mois2[5] = "Juin";
+mois2[6] = "Juillet";
+mois2[7] = "Août";
+mois2[8] = "Septembre";
+mois2[9] = "Octobre";
+mois2[10] = "Novembre";
+mois2[11] = "Décembre";
+console.table(mois2); 
+*/
+
+let weeks = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+console.log(weeks [4]);
+let weeks2 = new Array;
+
+
+let jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+console.log(jours[5]);
+
+
+let mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+for (let i = 0; 1<mois.length; i++) {
+    console.l
+
+    Let 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
